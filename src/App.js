594082d432cc3aac95212106c33596e60b9173a7
Codeyg12 <codey.gallup@gmail.com>
2023-11-15 19:51:44 -0700

    adding keys, refresh state, and refresh function

--- src/App.js
+++ src/App.js
@@ -1,31 +1,35 @@
 import { useState, useEffect } from 'react';
 import Button from 'react-bootstrap/Button'
+// https://tanstack.com/query/latest/docs/react/overview
 
 function App() {
-
   const [postData, setPostData] = useState([])
+  const [refresh, setRefresh] = useState(false)
 
-  
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/posts')
     .then(res => res.json())
     .then(data => {
       setPostData(data)
     })
-  }, []);
-  
-  console.log(postData);
+  }, [refresh]);
 
+  console.log(postData);
+  
   const postList = postData?.map(post => (
-    <li>{post.title}</li>
+    <li key={post.id}>{post.title}</li>
   ))
 
+  const refreshing = () => {
+    setRefresh(!refresh)
+  }
+
   return (
-    <>
+    <div className='text-center'>
     <h1 className="text-primary">Hello World!</h1>
     <ul>{postList}</ul>
-    <Button variant='info' className='text-white' onClick={() => console.log('working')}>Refresh Results</Button>
-    </>
+    <Button variant='info' className='text-white' onClick={refreshing}>Refresh Results</Button>
+    </div>
   );
 }
 
