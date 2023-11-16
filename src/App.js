dffff2838725780fbb4d01dc6282d0c180c6b10c
Codeyg12 <codey.gallup@gmail.com>
2023-11-15 19:42:10 -0700

    adding useState, mapping over api call and displaying to user

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Button from 'react-bootstrap/Button'
 
 function App() {
 
+  const [postData, setPostData] = useState([])
+
+  
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/posts')
     .then(res => res.json())
     .then(data => {
-      console.log(data);
+      setPostData(data)
     })
   }, []);
+  
+  console.log(postData);
+
+  const postList = postData?.map(post => (
+    <li>{post.title}</li>
+  ))
 
   return (
     <>
     <h1 className="text-primary">Hello World!</h1>
+    <ul>{postList}</ul>
     <Button variant='info' className='text-white' onClick={() => console.log('working')}>Refresh Results</Button>
     </>
   );
