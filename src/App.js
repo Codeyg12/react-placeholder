b37cb31194067567c03ac8067f8aaa0053292f49
Codeyg12 <codey.gallup@gmail.com>
2023-11-15 19:24:56 -0700

    testing components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import Button from 'react-bootstrap/Button'
 
 function App() {
   return (
     <>
     <h1 className="text-primary">Hello World!</h1>
+    <Button variant='info' className='text-white' onClick={() => console.log('working')}>Refresh Results</Button>
     </>
   );
 }
 
 export default App;
+  
