0e7c20b013e4aa6494e0cc5bccb35c1d09a95ceb
Codeyg12 <codey.gallup@gmail.com>
2023-11-15 19:30:41 -0700

    useEffect for inital call added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import { useEffect } from 'react';
 import Button from 'react-bootstrap/Button'
 
 function App() {
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+    })
+  }, []);
+
   return (
     <>
     <h1 className="text-primary">Hello World!</h1>
