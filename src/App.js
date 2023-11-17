d1bca9a043e105dac752ff9fb3ec5d38b2045303
Codeyg12 <codey.gallup@gmail.com>
2023-11-16 19:22:23 -0700

    creating PostList component and moving imports over

--- src/App.js
+++ src/App.js
@@ -1,37 +1,11 @@
-import { useState, useEffect } from 'react';
-import Button from 'react-bootstrap/Button'
-// https://tanstack.com/query/latest/docs/react/overview
+import PostList from "./components/PostList";
 
 function App() {
-  const [postData, setPostData] = useState([])
-  const [refresh, setRefresh] = useState(false)
-
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(res => res.json())
-    .then(data => {
-      setPostData(data)
-    })
-  }, [refresh]);
-
-  console.log(postData);
-  
-  const postList = postData?.map(post => (
-    <li key={post.id}>{post.title}</li>
-  ))
-
-  const refreshing = () => {
-    setRefresh(!refresh)
-  }
-
   return (
-    <div className='text-center'>
-    <h1 className="text-primary">Hello World!</h1>
-    <ul>{postList}</ul>
-    <Button variant='info' className='text-white' onClick={refreshing}>Refresh Results</Button>
-    </div>
+    <>
+      <PostList />
+    </>
   );
 }
 
 export default App;
-  
