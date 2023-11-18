3f59ac8155b96b46bcc1aa927aa73a24a675bc7f
Codeyg12 <codey.gallup@gmail.com>
2023-11-18 12:37:20 -0700

    updating store with thunk

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,5 +1,8 @@
-import { configureStore } from "@reduxjs/toolkit";
-
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import thunk from "redux-thunk";
 import reducer from "./reducer";
 
-export default configureStore({ reducer: reducer });
+export default configureStore({
+  reducer: reducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
+});
