2063c408fe32f84f43459b683c442c5ed4f97b7a
Codeyg12 <codey.gallup@gmail.com>
2023-11-18 13:15:31 -0700

    adding actions

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Button, Container, Row, Col } from "react-bootstrap";
 // https://tanstack.com/query/latest/docs/react/overview
 
