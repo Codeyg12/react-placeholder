f1ff4555cf07577fad6be4187807603f3292d327
Codeyg12 <codey.gallup@gmail.com>
2023-11-18 13:20:35 -0700

    adding reducers

--- src/utils/reducer.js
+++ src/utils/reducer.js
@@ -1,6 +1,31 @@
 const initalState = {
   posts: [],
   refresh: false,
+  error: null
 };
 
-const reducer = (state = initalState, action) => {};
+const reducer = (state = initalState, action) => {
+  switch (action.type) {
+    case 'FETCH_POSTS_REQUEST':
+      return {
+        ...state,
+        loading: true
+      }
+    case 'FETCH_POSTS_SUCCESS':
+      return {
+        ...state,
+        loading: false,
+        posts: action.payload
+      }
+    case 'FETCH_POSTS_FAIL':
+      return {
+        ...state,
+        loading: false,
+        error: action.payload
+      }
+    default: 
+    return state
+  }
+};
+
+export default reducer
