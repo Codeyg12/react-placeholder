ea9d1ff627a496ec9ba0d2533d640ac1b28ab304
Codeyg12 <codey.gallup@gmail.com>
2023-11-23 13:05:14 -0700

    updating styling

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchPosts } from "../utils/slice";
-import { Button, Container, Row, Col, Stack } from "react-bootstrap";
+import { Button, Container, Row, Col, Stack, Card } from "react-bootstrap";
 
 export default function PostList() {
   const dispatch = useDispatch();
@@ -18,21 +18,29 @@ export default function PostList() {
   if (error) {
     return <h1 className="text-center">Error: {error}</h1>;
   }
-
-  const postList = data?.map((post) => (
-    <Col key={post.id} md="3">
-      <h2>{post.title}</h2>
-      <p>{post.body}</p>
+  const postList = data?.map((post) => ( 
+    <Col key={post.id} md={3} className="mb-2">
+    <Card className="m-2" style={{ height: '100%'}}>
+      <Card.Header>
+        {post.title}
+      </Card.Header>
+      <Card.Body className="d-flex flex-column justify-content-center align-items-center">
+        <Card.Text>
+          {post.body}
+        </Card.Text>
+      </Card.Body>
+    </Card>
     </Col>
+
   ));
 
   return (
-    <Container className="text-center">
+    <Container fluid className="text-center">
       <Stack className="mx-auto col-md-4">
         <h1 className="text-primary">Post List</h1>
         <Button
           variant="info"
-          className="text-white"
+          className="text-white my-2"
           onClick={() => dispatch(fetchPosts())}
         >
           Refresh Results
@@ -41,7 +49,7 @@ export default function PostList() {
       <Row>{postList}</Row>
       <Button
         variant="info"
-        className="text-white col-md-4"
+        className="text-white col-md-4 my-2"
         onClick={() => dispatch(fetchPosts())}
       >
         Refresh Results
@@ -49,3 +57,64 @@ export default function PostList() {
     </Container>
   );
 }
+
+
+// import { useEffect } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { fetchPosts } from "../utils/slice";
+// import { Button, Container, Row, Col, Stack, Card } from "react-bootstrap";
+
+// export default function PostList() {
+//   const dispatch = useDispatch();
+//   const { data, loading, error } = useSelector((state) => state.posts);
+
+//   useEffect(() => {
+//     dispatch(fetchPosts());
+//   }, [dispatch]);
+
+//   if (loading) {
+//     return <h1 className="text-center">Loading...</h1>;
+//   }
+
+//   if (error) {
+//     return <h1 className="text-center">Error: {error}</h1>;
+//   }
+
+//   const postList = data?.map((post) => (
+//     <Col key={post.id} md={3}>
+//     <Card className="m-2" style={{ minHeight: '250px'}}>
+//       <Card.Header>
+//         {post.title}
+//       </Card.Header>
+//       <Card.Body>
+//         <Card.Text>
+//           {post.body}
+//         </Card.Text>
+//       </Card.Body>
+//     </Card>
+//     </Col>
+//   ));
+
+//   return (
+//     <Container className="text-center">
+//       <Stack className="mx-auto col-md-4">
+//         <h1 className="text-primary">Post List</h1>
+//         <Button
+//           variant="info"
+//           className="text-white"
+//           onClick={() => dispatch(fetchPosts())}
+//         >
+//           Refresh Results
+//         </Button>
+//       </Stack>
+//       <Row>{postList}</Row>
+//       <Button
+//         variant="info"
+//         className="text-white col-md-4"
+//         onClick={() => dispatch(fetchPosts())}
+//       >
+//         Refresh Results
+//       </Button>
+//     </Container>
+//   );
+// }
